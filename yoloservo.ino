0663a6e2a525ad718a19142745e338cf9602d10d
Syphon <siddiquishifa78@gmail.com>
2025-06-10 14:53:04 +0530

    uploaded detect.py, aurdino file

---
+++ yoloservo.ino
@@ -0,0 +1,43 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include <ESP32Servo.h>
+
+const char* ssid = "Airtel-MyWiFi-AMF-311WW-6EAB";
+const char* password = "2e541aa8";
+
+
+WebServer server(80);
+Servo myServo;
+
+void handleBanana() {
+  Serial.println("Banana Detected. Moving Servo.");
+  myServo.write(90);  // Move servo to 90 degrees
+  delay(2000);
+  myServo.write(0);   // Return to original position
+  server.send(200, "text/plain", "Servo Moved");
+}
+
+void setup() {
+  Serial.begin(115200);
+  myServo.attach(13);  // GPIO5 (D5), change as needed
+  myServo.write(0);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nConnected to WiFi.");
+  Serial.println(WiFi.localIP());
+
+  
+  server.on("/banana_detected", handleBanana);
+  server.on("/detect", HTTP_POST, handleBanana);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+  server.handleClient();
+}
